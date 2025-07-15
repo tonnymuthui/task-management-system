5ab367e2bcec0f49f904f2334347fb47afcdcbd7
tonnymuthui <69537407+tonnymuthui@users.noreply.github.com>
2025-07-15 04:26:16 +0300

    fix cors syntas in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,23 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-//
-const cors = require("cors");
-app.use(cors({ origin: "https://task-management-system-gray-xi.vercel.app/" }));
-
-//
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
-app.use(cors());
+
+
+app.use(cors({
+  origin: "https://task-management-system-gray-xi.vercel.app",
+  credentials: true
+}));
+
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
